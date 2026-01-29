b692077741ed30792963de86071f69802653334b
juniorpereira8717-cloud <juniorpereira8717@gmail.com>
2026-01-29 10:45:40 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const cacheName = 'resgate-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Instala o Service Worker e guarda os arquivos no cache
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Faz o jogo rodar offline buscando no cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
